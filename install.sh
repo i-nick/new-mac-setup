d484f2b3fbacf14cedaf3c494640f90c187bdf2a
i-nick <69675259+i-nick@users.noreply.github.com>
2026-04-02 00:28:49 -0700

    Modify install.sh for zerobrew and bun.sh
    Updated installation commands for zerobrew and added bun.sh installation.

--- install.sh
+++ install.sh
@@ -9,10 +9,14 @@ cd /opt/zerobrew
 sudo chmod 777 .
 # Installing zerobrew
 curl -fsSL https://raw.githubusercontent.com/i-nick/zerobrew/refs/heads/main/install.sh | bash
-zb init
+export ZEROBREW_DIR=$HOME/.local
+$ZEROBREW_DIR/bin/zb init
+source $HOME/.zshenv
 zb install ffmpeg jq yt-dlp hf
 zb install cask:ghostty cask:visual-studio-code cask:brave-browser cask:httpie-desktop cask:zed
 # Installing rustup
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+# Installing bun.sh
+curl -fsSL https://bun.sh/install | bash
 # Open iCloud sign-in
 open "x-apple.systempreferences:com.apple.preferences.AppleIDPrefPane"
